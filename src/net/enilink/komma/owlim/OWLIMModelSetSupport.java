d945d386266bbd20e5d7ac3ecb7a9402e1cfda4d
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2014-06-17 22:40:37 +0200

    Fixed handling of basic RDF and OWL knowledge in combination with reasoning.
    Change-Id: I9229d876afcf358c2b21bbb80f372990b165655e

--- src/net/enilink/komma/owlim/OWLIMModelSetSupport.java
+++ src/net/enilink/komma/owlim/OWLIMModelSetSupport.java
@@ -14,14 +14,29 @@ import info.aduna.io.FileUtil;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 
+import net.enilink.commons.iterator.IExtendedIterator;
 import net.enilink.composition.annotations.Iri;
 import net.enilink.komma.core.EntityVar;
+import net.enilink.komma.core.IReference;
+import net.enilink.komma.core.IStatement;
+import net.enilink.komma.core.IValue;
 import net.enilink.komma.core.InferencingCapability;
+import net.enilink.komma.core.KommaException;
+import net.enilink.komma.core.URI;
+import net.enilink.komma.core.URIs;
+import net.enilink.komma.dm.IDataManager;
+import net.enilink.komma.dm.IDataManagerFactory;
+import net.enilink.komma.dm.IDataManagerQuery;
+import net.enilink.komma.dm.change.IDataChangeSupport;
+import net.enilink.komma.internal.sesame.SesameRepositoryDataManager;
 import net.enilink.komma.model.MODELS;
 import net.enilink.komma.model.sesame.MemoryModelSetSupport;
+import net.enilink.komma.sesame.SesameDataManagerFactory;
+import net.enilink.komma.sesame.SesameModule;
 
 import org.openrdf.model.Model;
 import org.openrdf.model.Resource;
@@ -43,16 +58,80 @@ import org.openrdf.rio.helpers.StatementCollector;
 import org.openrdf.sail.config.SailRegistry;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
 import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.util.Modules;
 import com.ontotext.trree.owlim_ext.config.OWLIMSailFactory;
 
 @Iri(MODELS.NAMESPACE + "OwlimModelSet")
 public abstract class OWLIMModelSetSupport extends MemoryModelSetSupport {
 	EntityVar<RepositoryManager> repositoryManager;
 
+	static class OWLIMLiteDataManagerFactory extends SesameDataManagerFactory {
+		@Override
+		public IDataManager get() {
+			return injector.getInstance(OWLIMLiteDataManager.class);
+		}
+	}
+
+	static class OWLIMLiteDataManager extends SesameRepositoryDataManager {
+		@Inject
+		public OWLIMLiteDataManager(Repository repository,
+				IDataChangeSupport changeSupport) {
+			super(repository, changeSupport);
+		}
+
+		protected IReference[] addNull(boolean includeInferred,
+				IReference[] contexts) {
+			if (includeInferred) {
+				for (IReference ctx : contexts) {
+					if (ctx == null) {
+						return contexts;
+					}
+				}
+				contexts = Arrays.copyOf(contexts, contexts.length + 1);
+				contexts[contexts.length - 1] = null;
+			}
+			return contexts;
+		}
+
+		@Override
+		public <R> IDataManagerQuery<R> createQuery(String query,
+				String baseURI, boolean includeInferred, IReference... contexts) {
+			return super.createQuery(query, baseURI, includeInferred,
+					addNull(includeInferred, contexts));
+		}
+
+		@Override
+		public boolean hasMatch(IReference subject, IReference predicate,
+				IValue object, boolean includeInferred, IReference... contexts) {
+			return super.hasMatch(subject, predicate, object, includeInferred,
+					addNull(includeInferred, contexts));
+		}
+
+		@Override
+		public IExtendedIterator<IStatement> match(IReference subject,
+				IReference predicate, IValue object, boolean includeInferred,
+				IReference... contexts) {
+			return super.match(subject, predicate, object, includeInferred,
+					addNull(includeInferred, contexts));
+		}
+	}
+
 	@Override
 	public void collectInjectionModules(Collection<Module> modules) {
-		super.collectInjectionModules(modules);
+		modules.add(Modules.override(new SesameModule()).with(
+				new AbstractModule() {
+					@Override
+					protected void configure() {
+						bind(OWLIMLiteDataManagerFactory.class).in(
+								Singleton.class);
+						bind(IDataManagerFactory.class).to(
+								OWLIMLiteDataManagerFactory.class);
+					}
+				}));
 		modules.add(new AbstractModule() {
 			@Override
 			protected void configure() {
@@ -68,6 +147,16 @@ public abstract class OWLIMModelSetSupport extends MemoryModelSetSupport {
 								return true;
 							}
 						});
+			}
+
+			@Singleton
+			@Provides
+			protected Repository provideRepository() {
+				try {
+					return getBehaviourDelegate().createRepository();
+				} catch (RepositoryException e) {
+					throw new KommaException("Unable to create repository.", e);
+				}
 			}
 		});
 	}
@@ -124,5 +213,10 @@ public abstract class OWLIMModelSetSupport extends MemoryModelSetSupport {
 			man.shutDown();
 		}
 		repositoryManager.remove();
+	}
+
+	@Override
+	public URI getDefaultGraph() {
+		return URIs.createURI("komma:default");
 	}
 }
